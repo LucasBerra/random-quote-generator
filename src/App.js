9ae977eb3e335707158ead8400cbc825d5570f29
LucasBerra <berra.lucas.santiago@gmail.com>
2021-08-23 14:38:28 -0300

    Added background color changing.

--- src/App.js
+++ src/App.js
@@ -39,6 +39,27 @@ const Cards = () => {
 
   generateQuote();
 
+  const bgColors = [
+    "rgb(172, 80, 80)",
+    "rgb(197, 113, 64)",
+    "rgb(177, 159, 59)",
+    "rgb(120, 168, 74)",
+    "rgb(74, 168, 82)",
+    "rgb(53, 163, 121)",
+    "rgb(59, 160, 155)",
+    "rgb(59, 94, 160)",
+    "rgb(73, 74, 165)",
+    "rgb(111, 82, 167)",
+    "rgb(143, 59, 160)",
+    "rgb(160, 59, 106)",
+  ];
+  const changeBgColor = () => {
+    let colorIndex = Math.floor(Math.random() * bgColors.length);
+    let currentBgColor = bgColors[colorIndex];
+
+    document.querySelector("body").style.backgroundColor = currentBgColor;
+  };
+
   return (
     <>
       <div id="quote-container">
@@ -51,7 +72,14 @@ const Cards = () => {
         </div>
         <h3>- {author}</h3>
       </div>
-      <button onClick={newQuote}>generate quote</button>
+      <button
+        onClick={() => {
+          newQuote();
+          changeBgColor();
+        }}
+      >
+        generate quote
+      </button>
     </>
   );
 };
