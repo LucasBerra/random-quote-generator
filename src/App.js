056e644c7127bc285571585eb3dcb1ebd09e3fe8
LucasBerra <berra.lucas.santiago@gmail.com>
2021-08-23 13:30:58 -0300

    Uploaded main site to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { quotes } from "./quotes";
+import { useState } from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>- Quote Generator -</h1>
+      <div id="card-target">
+        <Cards />
+      </div>
+    </main>
   );
-}
+};
+
+const Cards = () => {
+  const randomIndex = () => {
+    return Math.floor(Math.random() * quotes.length);
+  };
+
+  let [index, setIndex] = useState(randomIndex());
+
+  const newQuote = () => {
+    setIndex((index = randomIndex()));
+  };
+
+  let text;
+  let author;
+
+  const generateQuote = () => {
+    text = quotes[index]["text"];
+    author = quotes[index]["author"];
+
+    if (author === null) {
+      author = "Unknown author";
+    }
+  };
+
+  generateQuote();
+
+  return (
+    <>
+      <div id="quote-container">
+        <div id="quote-text">
+          <span>"</span>
+          <p>
+            <em>{text}</em>
+          </p>
+          <span>"</span>
+        </div>
+        <h3>- {author}</h3>
+      </div>
+      <button onClick={newQuote}>generate quote</button>
+    </>
+  );
+};
 
 export default App;
